c97349e64083e268b32bb24325f1450618542286
benpark0613 <benpark0613@outlook.com>
2025-01-04 21:00:14 +0900

    jpa 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -17,9 +17,10 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
-@Slf4j
+//@Slf4j
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
